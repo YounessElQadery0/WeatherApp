87fb8275390a8919cb106ae032d66e71f663f783
YounessElQadery0 <158784197+YounessElQadery0@users.noreply.github.com>
2024-05-31 03:41:33 +0200

    Initial commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Youness El Qadery on 2024-03-17.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+           ContentView()
+        }
+    }
+}
